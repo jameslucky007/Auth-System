4be2c1f1ffa5fd36f8d2ada652a7a2f8514b8e80
Lucky A <105838230+jameslucky007@users.noreply.github.com>
2025-07-15 21:17:12 +0530

    Initial commit

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,8 +1,11 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+const page = () => {
   return (
-  
-  
-  );
+    <>
+      
+    </>
+  )
 }
+
+export default page
